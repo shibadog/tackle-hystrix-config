dc33771371cd96b0a669d232ec3466ed059f6585
shibadog <shiba_dog@outlook.com>
2019-01-09 15:44:36 +0900

    hystrixのmetricをJMXでとれるか試す（ダメぽ

--- src/main/java/jp/shibadog/tackle/hystrixconfig/Application.java
+++ src/main/java/jp/shibadog/tackle/hystrixconfig/Application.java
@@ -1,16 +1,31 @@
 package jp.shibadog.tackle.hystrixconfig;
 
+import com.netflix.hystrix.contrib.servopublisher.HystrixServoMetricsPublisher;
+import com.netflix.hystrix.strategy.HystrixPlugins;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
+@EnableScheduling
 @EnableCircuitBreaker
 @SpringBootApplication
 @EnableAsync
-public class Application {
+public class Application extends SpringBootServletInitializer {
+    static {
+        HystrixPlugins.getInstance().registerMetricsPublisher(HystrixServoMetricsPublisher.getInstance());
+    }
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
